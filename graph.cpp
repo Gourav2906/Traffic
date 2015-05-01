8e1b5996a82ecb2d10fc59a2429c4b2360810aaf
Gourav2906 <ggarg2906@gmail.com>
2015-05-02 00:28:59 +0530

    modification

--- graph.cpp
+++ graph.cpp
@@ -2,3 +2,74 @@
 
 using namespace std;
 
+struct node
+{
+	int a;
+	struct node *next;
+};
+
+
+struct list
+{
+	struct node *head;
+};
+
+
+struct Graph
+{
+	int v;
+	struct list * array;
+};
+
+
+struct node * newnode(int data)
+{
+	struct node* newmode=new struct node;
+    newmode->a=data;
+    newmode->next=NULL;
+	return newmode;
+}
+
+struct Graph *creategraph(int v)
+{
+	struct Graph * graph=new Graph;
+	
+	graph->v=v;
+	
+	graph->array=new list;
+	
+    int i;
+    
+    for(i=0;i<v;i++)
+    graph->array[i].head=NULL;
+    
+    return graph;
+}
+
+void addedge(struct Graph* graph,int src,int dest)
+{
+	struct node* newmode=newnode(dest);
+	newmode->next =graph->array[src].head;
+	
+	graph->array[src].head=newmode;
+	
+	
+	newmode=newnode(src);
+	
+	newmode->next=graph->array[dest].head;
+	
+	graph->array[dest].head=newmode;
+	
+	
+}
+
+
+
+
+
+int main()
+{
+	
+	
+return 0;	
+}
